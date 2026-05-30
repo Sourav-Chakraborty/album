f67a8061f239a165794851856890f3898d3768b4
sourav <sourav.chakraborty@intglobal.com>
2026-05-30 23:37:30 +0530

    feat: initialize Prisma ORM with MariaDB adapter, configuration, and initial User/Media schema migration

--- album-backend/src/lib/prisma.ts
+++ album-backend/src/lib/prisma.ts
@@ -3,10 +3,12 @@ import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client";
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT) || 4000,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
   connectionLimit: 5,
+  ssl: {},
 });
 const prisma = new PrismaClient({ adapter });
 export { prisma };
